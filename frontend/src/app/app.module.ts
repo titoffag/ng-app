affbfaabb2ab9576fa4e0c1b5aaba79760e2aa04
Aleksandr_Titov <aleksandr_titov@epam.com>
2019-12-20 13:38:37 +0400

    added ngrx packages in project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { EntityDataModule } from '@ngrx/data';
 
 import { AppRoutingModule } from '@views/app-routing.module';
 import { HeaderComponentModule } from '@components/header/header.component';
 import { FooterComponentModule } from '@components/footer/footer.component';
 import { LoadingBlockModule } from '@components/loading-block/loading-block.component';
+import { environment } from 'src/environments/environment';
 
 import { AppComponent } from './app.component';
+import { reducers, metaReducers } from './reducers';
+import { entityConfig } from './entity-metadata';
+import { AppEffects } from './app.effects';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,7 +28,22 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HeaderComponentModule,
     FooterComponentModule,
-    LoadingBlockModule
+    LoadingBlockModule,
+    FooterComponentModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
+    EffectsModule.forRoot([AppEffects]),
+    StoreRouterConnectingModule.forRoot(),
+    EntityDataModule.forRoot(entityConfig)
   ],
   bootstrap: [AppComponent]
 })
